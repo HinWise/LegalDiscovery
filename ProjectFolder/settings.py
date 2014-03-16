c8b855f2d0fcecce4bd4d8c406967769950dcbf3
Xantomen <xantomen@gmail.com>
2014-03-16 13:43:30 +0100

    Change IP

--- ProjectFolder/settings.py
+++ ProjectFolder/settings.py
@@ -288,7 +288,7 @@ if DEBUG:
     if socket.gethostname() == 'Nemot1' or socket.gethostname() == 'Marios-PC' or socket.gethostname() == 'Nemot-PC':
         INTERNAL_IPS = ('127.0.0.1',)
     else:
-        INTERNAL_IPS = ('31.7.58.66',)
+        INTERNAL_IPS = ('46.19.138.162',)
        
     MIDDLEWARE_CLASSES += (
         'debug_toolbar.middleware.DebugToolbarMiddleware',
